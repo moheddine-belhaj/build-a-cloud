7a09681e9d43c9b126594274c79ab13d361cf8ef
moheddine-belhaj <moheddinebelhaj@gmail.com>
2026-07-29 14:24:45 +0200

    Add ArgoCD application manifest

--- week-2/infra/main.tf
+++ week-2/infra/main.tf
@@ -9,9 +9,9 @@ resource "stackit_ske_cluster" "ske_cluster" {
 
   node_pools = [{
     name               = "default"
-    machine_type       = "g1a.4d"
+    machine_type       = "g1a.2d"
     minimum            = 2
-    maximum            = 4
+    maximum            = 5
     availability_zones = ["eu01-1", "eu01-2", "eu01-3"]
     os_name            = "flatcar"
     volume_size        = 32
