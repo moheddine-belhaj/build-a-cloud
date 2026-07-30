de5346a392314058ee0d5ebd67cb74fb28076632
moheddine-belhaj <moheddinebelhaj@gmail.com>
2026-07-30 19:47:32 +0200

    Update deploy trigger

--- week-2/infra/main.tf
+++ week-2/infra/main.tf
@@ -2,7 +2,7 @@
 # Creates a Kubernetes cluster named "paas-ske" with a single node pool.
 # - `kubernetes_version_min`: Minimum K8s version
 # - `node_pools`: Defines machine type, scaling and storage
-# retrigger deploy 2
+# retrigger deploy 3
 
 resource "stackit_ske_cluster" "ske_cluster" {
   project_id             = var.project_id
