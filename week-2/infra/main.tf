70bb07c8eaa8def220b89d73f51a66337c3f590b
moheddine-belhaj <moheddinebelhaj@gmail.com>
2026-07-30 18:49:32 +0200

    debug

--- week-2/infra/main.tf
+++ week-2/infra/main.tf
@@ -2,7 +2,7 @@
 # Creates a Kubernetes cluster named "paas-ske" with a single node pool.
 # - `kubernetes_version_min`: Minimum K8s version
 # - `node_pools`: Defines machine type, scaling and storage
-# retrigger deploy : End-to-end pipeline verification
+# retrigger deploy : debug
 
 resource "stackit_ske_cluster" "ske_cluster" {
   project_id             = var.project_id
